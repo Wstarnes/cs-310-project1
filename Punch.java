804e210508bae1fcd21a713057d5cd906b6c14d3
deezworld2 <35939295+deezworld2@users.noreply.github.com>
2018-03-09 11:28:20 -0600

    Punch class completed (F1 stage --untested)
    I finished writing all of the initial code for the class, but I have not run any tests to make sure that the class works as intended.

--- Punch.java
+++ Punch.java
@@ -1,16 +1,30 @@
 package cs310project1;
 
 import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class Punch {
-    
+    private Badge badge;
+    private int punchid;
+    private int terminalid;
+    private int punchtypeid;
+    private GregorianCalendar stamp;
+
     public Punch(Badge badge, int terminalid, int punchtypeid){
-        
+        this.badge = badge;
+        this.terminalid = terminalid;
+        this.punchtypeid = punchtypeid;
+        this.stamp = new GregorianCalendar(TimeZone.getDefault());
     }
 
+<<<<<<< HEAD
+    public GregorianCalendar printOriginalTimeStampp(){
+        if (punchtypeid == 0)
+=======
     Object printOriginalTimestamp() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     
+>>>>>>> c1bdee4993c71b52cca6fc64ffa73fbde8a64e42
 }
